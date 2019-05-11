95ee126c10a5d8e718efd3adf79293b9561c8052
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-05-11 14:12:13 +0300

    Make a few small changes in the movie form
     - Move the close button
    
     - Add an IMDb link below the poster

--- MovieList/Views/Properties.cs
+++ MovieList/Views/Properties.cs
@@ -1,22 +1,47 @@
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Navigation;
 
 namespace MovieList.Views
 {
     public static class Properties
     {
         public static readonly DependencyProperty TripleClickSelectAllProperty = DependencyProperty.RegisterAttached(
-            "TripleClickSelectAll", typeof(bool), typeof(Properties), new PropertyMetadata(false, OnPropertyChanged));
+            "TripleClickSelectAll",
+            typeof(bool),
+            typeof(Properties),
+            new PropertyMetadata(false, OnTripleClickSelectAllChanged));
 
-        private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        public static readonly DependencyProperty IsExternalProperty = DependencyProperty.RegisterAttached(
+            "IsExternal",
+            typeof(bool),
+            typeof(Properties),
+            new UIPropertyMetadata(false, OnIsExternalChanged));
+
+        public static bool GetTripleClickSelectAll(DependencyObject element)
+            => (bool)element.GetValue(TripleClickSelectAllProperty);
+
+        public static void SetTripleClickSelectAll(DependencyObject element, bool value)
+            => element.SetValue(TripleClickSelectAllProperty, value);
+
+        public static bool GetIsExternal(DependencyObject obj)
+            => (bool)obj.GetValue(IsExternalProperty);
+
+        public static void SetIsExternal(DependencyObject obj, bool value)
+            => obj.SetValue(IsExternalProperty, value);
+
+        private static void OnTripleClickSelectAllChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is TextBox textBox && e.NewValue is bool enable)
             {
                 if (enable)
                 {
                     textBox.PreviewMouseLeftButtonDown += OnTextBoxMouseDown;
-                } else
+                }
+                else
                 {
                     textBox.PreviewMouseLeftButtonDown -= OnTextBoxMouseDown;
                 }
@@ -31,10 +56,24 @@ namespace MovieList.Views
             }
         }
 
-        public static void SetTripleClickSelectAll(DependencyObject element, bool value)
-            => element.SetValue(TripleClickSelectAllProperty, value);
+        private static void OnIsExternalChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            if (sender is Hyperlink hyperlink)
+            {
+                if ((bool)args.NewValue)
+                {
+                    hyperlink.RequestNavigate += OnHyperlinkRequestNavigate;
+                } else
+                {
+                    hyperlink.RequestNavigate -= OnHyperlinkRequestNavigate;
+                }
+            }
+        }
 
-        public static bool GetTripleClickSelectAll(DependencyObject element)
-            => (bool)element.GetValue(TripleClickSelectAllProperty);
+        private static void OnHyperlinkRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
     }
 }
