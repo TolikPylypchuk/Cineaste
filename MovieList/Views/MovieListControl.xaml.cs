e823243fc4182375389140c27ca7b8cbee33728e
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-05-22 20:16:52 +0300

    Add the ability to switch list items with a keyboard

--- MovieList/Views/MovieListControl.xaml.cs
+++ MovieList/Views/MovieListControl.xaml.cs
@@ -23,6 +23,36 @@ namespace MovieList.Views
                         this.LoadingProgressBar.Visibility = Visibility.Collapsed;
                     }));
 
+        private void MovieListControl_PreviewKeyDown(object sender, KeyEventArgs e)
+            => e.Handled = e.Key == Key.Up || e.Key == Key.Down;
+
+        private void MovieListControl_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (this.List.SelectedItem != null)
+            {
+                if (e.Key == Key.Up)
+                {
+                    this.List.SelectedIndex = (this.List.SelectedIndex + this.ViewModel.Items.Count - 1) % this.ViewModel.Items.Count;
+                    e.Handled = true;
+                } else if (e.Key == Key.Down)
+                {
+                    this.List.SelectedIndex = (this.List.SelectedIndex + this.ViewModel.Items.Count + 1) % this.ViewModel.Items.Count;
+                    e.Handled = true;
+                }
+            } else
+            {
+                if (e.Key == Key.Up)
+                {
+                    this.List.SelectedIndex = this.ViewModel.Items.Count - 1;
+                    e.Handled = true;
+                } else if (e.Key == Key.Down)
+                {
+                    this.List.SelectedIndex = 0;
+                    e.Handled = true;
+                }
+            }
+        }
+
         private void MoreButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (sender is Button button)
