5389e1c83087a64e9018250b58b8089d86a66e89
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-09-24 21:01:20 +0300

    Create projects for data and view models
    Clean up the initial code in the main project. Create 2 new projects:
    for data and for view models. Set the C# version to 8.0 in all projects
    and enable nullable reference types.
    
     #8

--- MovieList/App.xaml.cs
+++ MovieList/App.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace MovieList
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
