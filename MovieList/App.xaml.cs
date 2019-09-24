0ab524647e4e21af8c1ac81687ec642093adeedd
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-09-24 20:33:02 +0300

    Remove everything and start anew
    Remove the whole MovieList solution. Create a new one. The new solution
    will use ReactiveUI instead of custom MVVM and Material Design instead
    of HandyControl.
    
    This commit contains the solution as it was generated by Visual Studio.
    
     #8

--- MovieList/App.xaml.cs
+++ MovieList/App.xaml.cs
@@ -1,81 +1,17 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
-
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-using MovieList.Config;
-using MovieList.Data;
-using MovieList.Options;
-using MovieList.Services;
-using MovieList.Services.Implementations;
-using MovieList.ViewModels;
-using MovieList.Views;
 
 namespace MovieList
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
-        public IServiceProvider ServiceProvider { get; private set; }
-        public IConfigurationRoot Configuration { get; private set; }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            this.Configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            var services = new ServiceCollection();
-            this.ConfigureServices(services);
-            this.ServiceProvider = services.BuildServiceProvider();
-
-            AppDomain.CurrentDomain.UnhandledException += this.CatchException;
-
-            this.MainWindow = this.ServiceProvider.GetRequiredService<MainWindow>();
-            this.MainWindow?.Show();
-        }
-
-        private void ConfigureServices(IServiceCollection services)
-            => services
-                .AddDbContext<MovieContext>(
-                    (s, builder) => builder
-                        .ConfigureMovieContext(this.Configuration.GetSection("Config")["DatabasePath"])
-                        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-                        .EnableSensitiveDataLogging()
-                        .EnableDetailedErrors(),
-                    ServiceLifetime.Transient)
-
-                .AddLogging(loggingBuilder => loggingBuilder
-                    .AddConfiguration(this.Configuration.GetSection("Logging"))
-                    .AddFile(this.Configuration.GetSection("Logging")))
-
-                .ConfigureWritable<Configuration>(this.Configuration.GetSection("Config"))
-                .ConfigureWritable<UIConfig>(this.Configuration.GetSection("UI"))
-                .ConfigureWritable<LoggingConfig>(this.Configuration.GetSection("Logging"))
-
-                .AddSingleton<IDbService, DbService>()
-                .AddSingleton<IFileService, FileService>()
-
-                .AddSingleton<MainViewModel>()
-                .AddSingleton<MovieListViewModel>()
-                .AddSingleton<SidePanelViewModel>()
-                .AddSingleton<AddNewViewModel>()
-                .AddSingleton<MovieFormViewModel>()
-                .AddSingleton<SeriesFormViewModel>()
-                .AddSingleton<MovieSeriesFormViewModel>()
-                .AddSingleton<SeasonFormViewModel>()
-                .AddSingleton<SpecialEpisodeFormViewModel>()
-                .AddSingleton<SettingsViewModel>()
-
-                .AddSingleton<MainWindow>()
-                .AddSingleton(_ => this.ServiceProvider)
-                .AddSingleton(_ => this);
-
-        private void CatchException(object sender, UnhandledExceptionEventArgs e)
-            => this.ServiceProvider.GetService<ILogger<App>>().LogError(e.ExceptionObject.ToString());
     }
 }
--- MovieList/Services/IFileService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace MovieList.Services
-{
-    public interface IFileService
-    {
-        ValueTask<bool> TryMoveFileAsync(string source, string destination);
-    }
-}
