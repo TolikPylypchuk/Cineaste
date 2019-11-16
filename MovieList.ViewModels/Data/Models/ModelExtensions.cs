98c212028232f1d0712a6d4df72fcc19c6326a0b
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-11-16 19:31:20 +0200

    Implement comparing list items
    The list items follow a quite difficult comparison algorithm. Implement
    a comparer class which does the comparing, as opposed to the items
    themselves being comparable (the way it was implemented previously).
    
    Add normalization when comparing titles. It includes making the text
    case-insensitive and removing colons and dashes.
    
     #19

--- MovieList.ViewModels/Data/Models/ModelExtensions.cs
+++ MovieList.ViewModels/Data/Models/ModelExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MovieList.Data.Models
@@ -32,10 +33,69 @@ namespace MovieList.Data.Models
             return firstEntry.Movie?.GetActiveColor() ?? firstEntry.Series!.GetActiveColor();
         }
 
+        public static Title GetTitle(this MovieSeries movieSeries)
+            => movieSeries.Title ?? movieSeries.GetFirstEntry().GetTitle();
+
+        public static Title GetTitle(this MovieSeriesEntry entry)
+            => entry.Movie != null
+                ? entry.Movie.Title
+                : entry.Series != null
+                    ? entry.Series.Title
+                    : entry.MovieSeries!.GetTitle();
+
         public static MovieSeriesEntry GetFirstEntry(this MovieSeries movieSeries)
         {
             var firstEntry = movieSeries.Entries.OrderBy(entry => entry.SequenceNumber).First();
             return firstEntry.MovieSeries == null ? firstEntry : firstEntry.MovieSeries.GetFirstEntry();
+        }
+
+        public static MovieSeriesEntry GetLastEntry(this MovieSeries movieSeries)
+        {
+            var lastEntry = movieSeries.Entries.OrderByDescending(entry => entry.SequenceNumber).First();
+            return lastEntry.MovieSeries == null ? lastEntry : lastEntry.MovieSeries.GetLastEntry();
+        }
+
+        public static MovieSeries GetRootSeries(this MovieSeries movieSeries)
+            => movieSeries.Entry == null ? movieSeries : movieSeries.Entry.ParentSeries.GetRootSeries();
+
+        public static bool IsDescendantOf(this MovieSeries? series, MovieSeries potentialAncestor)
+            => series != null &&
+                (series.Id == potentialAncestor.Id ||
+                    (series.Entry?.ParentSeries.IsDescendantOf(potentialAncestor) ?? false));
+
+        public static IEnumerable<MovieSeries> GetAllAncestors(this MovieSeries? series)
+        {
+            if (series == null)
+            {
+                yield break;
+            }
+
+            if (series.Entry != null)
+            {
+                foreach (var ancestor in series.Entry.ParentSeries.GetAllAncestors())
+                {
+                    yield return ancestor;
+                }
+            }
+
+            yield return series;
+        }
+
+        public static (MovieSeries, MovieSeries) GetDistinctAncestors(this MovieSeries series1, MovieSeries series2)
+            => series1.GetAllAncestors()
+                .Zip(series2.GetAllAncestors(), (a, b) => (Series1: a, Series2: b))
+                .First(ancestors => ancestors.Series1.Id != ancestors.Series2.Id);
+
+        public static int GetStartYear(this MovieSeries movieSeries)
+        {
+            var firstEntry = movieSeries.GetFirstEntry();
+            return firstEntry.Movie?.Year ?? firstEntry.Series!.StartYear;
+        }
+
+        public static int GetEndYear(this MovieSeries movieSeries)
+        {
+            var lastEntry = movieSeries.GetLastEntry();
+            return lastEntry.Movie?.Year ?? lastEntry.Series!.EndYear;
         }
     }
 }
---
+++ MovieList.ViewModels/ListItems/ListItemComparer.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+using MovieList.Data.Models;
+
+namespace MovieList.ListItems
+{
+    public class ListItemComparer : IComparer<ListItem>
+    {
+        private ListItemComparer() { }
+
+        public static ListItemComparer Instance { get; } = new ListItemComparer();
+
+        public int Compare([AllowNull] ListItem x, [AllowNull] ListItem y)
+            => (x, y) switch
+            {
+                (null, null) => 0,
+                (null, _) => -1,
+                (_, null) => 1,
+                (MovieListItem left, MovieListItem right) => this.Compare(left, right),
+                (MovieListItem left, SeriesListItem right) => this.Compare(left, right),
+                (MovieListItem left, MovieSeriesListItem right) => this.Compare(left, right),
+                (SeriesListItem left, MovieListItem right) => this.Compare(left, right),
+                (SeriesListItem left, SeriesListItem right) => this.Compare(left, right),
+                (SeriesListItem left, MovieSeriesListItem right) => this.Compare(left, right),
+                (MovieSeriesListItem left, MovieListItem right) => this.Compare(left, right),
+                (MovieSeriesListItem left, SeriesListItem right) => this.Compare(left, right),
+                (MovieSeriesListItem left, MovieSeriesListItem right) => this.Compare(left, right),
+                _ => throw new NotSupportedException(
+                    $"Types of list items to compare are not supported: {x.GetType()}, {y.GetType()}")
+            };
+
+        private int Compare(MovieListItem left, MovieListItem right)
+            => left.Movie.Id == right.Movie.Id
+                ? 0
+                : this.CompareEntries(left, right, left.Movie.Entry, right.Movie.Entry);
+
+        private int Compare(MovieListItem left, SeriesListItem right)
+            => this.CompareEntries(left, right, left.Movie.Entry, right.Series.Entry);
+
+        private int Compare(MovieListItem left, MovieSeriesListItem right)
+            => this.Compare(right, left) * -1;
+
+        private int Compare(SeriesListItem left, MovieListItem right)
+            => this.CompareEntries(left, right, left.Series.Entry, right.Movie.Entry);
+
+        private int Compare(SeriesListItem left, SeriesListItem right)
+            => left.Series.Id == right.Series.Id
+                ? 0
+                : this.CompareEntries(left, right, left.Series.Entry, right.Series.Entry);
+
+        private int Compare(SeriesListItem left, MovieSeriesListItem right)
+            => this.Compare(right, left) * -1;
+
+        private int Compare(MovieSeriesListItem left, MovieListItem right)
+            => this.CompareEntries(left, right, right.Movie.Entry);
+
+        private int Compare(MovieSeriesListItem left, SeriesListItem right)
+            => this.CompareEntries(left, right, right.Series.Entry);
+
+        private int Compare(MovieSeriesListItem left, MovieSeriesListItem right)
+        {
+            int result;
+
+            if (left.MovieSeries.Id == right.MovieSeries.Id)
+            {
+                result = 0;
+            } else if (left.MovieSeries.IsDescendantOf(right.MovieSeries))
+            {
+                result = 1;
+            } else if (right.MovieSeries.IsDescendantOf(left.MovieSeries))
+            {
+                result = -1;
+            } else if (left.MovieSeries.GetRootSeries().Id == right.MovieSeries.GetRootSeries().Id)
+            {
+                var (ancestor1, ancestor2) = left.MovieSeries.GetDistinctAncestors(right.MovieSeries);
+                result = ancestor1.Entry?.SequenceNumber.CompareTo(ancestor2.Entry?.SequenceNumber) ?? 0;
+            } else
+            {
+                result = left.MovieSeries.GetTitle().Name.CompareTo(right.MovieSeries.GetTitle().Name);
+
+                if (result == 0)
+                {
+                    result = left.MovieSeries.GetStartYear().CompareTo(right.MovieSeries.GetStartYear());
+                }
+            }
+
+            return result;
+        }
+
+#pragma warning disable 8509
+
+        private int CompareEntries(
+            ListItem left,
+            ListItem right,
+            MovieSeriesEntry? thisEntry,
+            MovieSeriesEntry? otherEntry)
+            => (thisEntry, otherEntry) switch
+            {
+                (null, null) => this.CompareTitleOrYear(left, right),
+                (var entry, null) => this.Compare(new MovieSeriesListItem(entry.ParentSeries), right),
+                (null, var entry) => this.Compare(left, new MovieSeriesListItem(entry.ParentSeries)),
+                var (entry1, entry2) => entry1.MovieSeriesId == entry2.MovieSeriesId
+                    ? entry1.SequenceNumber.CompareTo(entry2.SequenceNumber)
+                    : this.Compare(
+                        new MovieSeriesListItem(entry1.ParentSeries),
+                        new MovieSeriesListItem(entry2.ParentSeries))
+            };
+
+#pragma warning restore 8509
+
+        private int CompareEntries(MovieSeriesListItem left, ListItem right, MovieSeriesEntry? entry)
+            => entry == null
+                ? this.CompareTitleOrYear(left, right)
+                : entry.ParentSeriesId == left.MovieSeries.Id
+                    ? -1
+                    : this.Compare(left, new MovieSeriesListItem(entry.ParentSeries));
+
+        private int CompareTitleOrYear(ListItem left, ListItem right)
+        {
+            int result = TitleComparer.Instance.Compare(left.Title, right.Title);
+            return result != 0 ? result : left.Year.CompareTo(right.Year);
+        }
+
+        private int CompareTitleOrYear(MovieSeriesListItem left, ListItem right)
+        {
+            int result = TitleComparer.Instance.Compare(left.MovieSeries.GetTitle().Name, right.Title);
+            return result != 0 ? result : left.Year.CompareTo(right.Year);
+        }
+    }
+}
