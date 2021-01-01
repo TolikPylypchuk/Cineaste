f6ed01cbfe748d59da6738558af3deaab3b97bef
TolikPylypchuk <TolikPylypchuk@users.noreply.github.com>
2021-01-01 16:15:49 +0000

    jekyll build from Action 38d74f1ba701384ea5c3f16c2152f6a3bc184d5e

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,145 @@
+var store = [{
+        "title": "Movie List",
+        "excerpt":"An app which keeps track of the movies and TV series you have watched or would like to watch. If you want to use this app to track movies and series, you can certainly do so, but bear in mind that this is a personal project - I’m writing this...","categories": [],
+        "tags": [],
+        "url": "/v0.1/",
+        "teaser": null
+      },{
+        "title": "Getting Started",
+        "excerpt":"How to Get the App The Movie List app works only on Windows (and only tested on Windows 10). Currently the only way to get this app is to download it from the ‘releases’ section on GitHub and extract it manually. The latest version is 0.2. Files and Tabs The...","categories": [],
+        "tags": [],
+        "url": "/v0.1/getting-started",
+        "teaser": null
+      },{
+        "title": "List Files",
+        "excerpt":"Here’s what the app looks like when you create a new file: The file tabs are composed of three main sections: the list itself, stats, and settings. You can switch between them using the left-side bar. When you open a file, you can close it via the ‘close’ button in...","categories": [],
+        "tags": [],
+        "url": "/v0.1/list-files",
+        "teaser": null
+      },{
+        "title": "Forms",
+        "excerpt":"You can add, edit, and delete list items through forms which appear on the right side of the main section. There are six types of forms: Movie Series Season (part of the series form) Special episode (part of the series form) Miniseries Franchise All list items can contain posters. None...","categories": [],
+        "tags": [],
+        "url": "/v0.1/forms",
+        "teaser": null
+      },{
+        "title": "The Movie Form",
+        "excerpt":"The movie form is the simplest and most straightforward of the forms. Here’s how it looks: The form consists of the following sections: Header which contains the first title, the ‘close’ button, and a few other buttons if the movie is a part of a franchise (which are discussed in...","categories": [],
+        "tags": [],
+        "url": "/v0.1/movie-form",
+        "teaser": null
+      },{
+        "title": "The Series Form",
+        "excerpt":"This is a big one. It takes much more effort to add a series than it is to add a movie. Here’s how a series form looks: The series form consists of the following parts: Header which contains the first title, the ‘close’ button, and a few other buttons if...","categories": [],
+        "tags": [],
+        "url": "/v0.1/series-form",
+        "teaser": null
+      },{
+        "title": "The Miniseries Form",
+        "excerpt":"The miniseries form describes series which are limited in nature. In the context of this app, a miniseries has only one season, which in turn has only one release period, and no special episodes. Here’s how a miniseries form looks: As you can see, the miniseries forms are much simpler...","categories": [],
+        "tags": [],
+        "url": "/v0.1/miniseries-form",
+        "teaser": null
+      },{
+        "title": "The Franchise Form",
+        "excerpt":"Lots of movies and series have multiple parts, so it’s logical to have these movies/series be grouped together in the list. This is just what franchises are for: grouping related movies, series or even other franchises. Franchises can be created and edited via the franchise forms, but those can’t be...","categories": [],
+        "tags": [],
+        "url": "/v0.1/franchise-form",
+        "teaser": null
+      },{
+        "title": "Preferences",
+        "excerpt":"The final feature covered in these docs is the app-wide preferences. You can open them through the Edit &gt; Preferences menu item, or by pressing Ctrl+Shift+P, and close them by clicking the ‘close’ button in the tab header, through the File &gt; Close menu item, or by pressing Ctrl+W. Here’s...","categories": [],
+        "tags": [],
+        "url": "/v0.1/preferences",
+        "teaser": null
+      },{
+        "title": "Technical Stuff",
+        "excerpt":"The Movie List app is written with C# and .NET Core 3.1. Following is the list of technologies used in this app and some other technical aspects. UI This app is a desktop app. I’ve contemplated writing a web app, but decided against it, partly because it’s easier, and because...","categories": [],
+        "tags": [],
+        "url": "/v0.1/technical",
+        "teaser": null
+      },{
+        "title": "About Movie List",
+        "excerpt":"A long time ago I started keeping a list of movies and TV series I’ve watched and would like to watch. Keeping track of all these movies and series in a Word document because cumbersome since now there are more than a thousand items (I watch a lot of movies)....","categories": [],
+        "tags": [],
+        "url": "/v0.1/about",
+        "teaser": null
+      },{
+        "title": "Movie List",
+        "excerpt":"An app which keeps track of the movies and TV series you have watched or would like to watch. If you want to use this app to track movies and series, you can certainly do so, but bear in mind that this is a personal project - I’m writing this...","categories": [],
+        "tags": [],
+        "url": "/v0.2/",
+        "teaser": null
+      },{
+        "title": "Getting Started",
+        "excerpt":"How to Get the App The Movie List app works only on Windows 10 May 2020 Update or later. Currently the only way to get this app is to download it from the ‘releases’ section on GitHub and extract it manually. The latest version is 0.2. Files and Tabs The...","categories": [],
+        "tags": [],
+        "url": "/v0.2/getting-started",
+        "teaser": null
+      },{
+        "title": "List Files",
+        "excerpt":"Here’s what the app looks like when you create a new file: The file tabs are composed of three main sections: the list itself, stats, and settings. You can switch between them using the left-side bar. When you open a file, you can close it via the ‘close’ button in...","categories": [],
+        "tags": [],
+        "url": "/v0.2/list-files",
+        "teaser": null
+      },{
+        "title": "Forms",
+        "excerpt":"You can add, edit, and delete list items through forms which appear on the right side of the main section. There are six types of forms: Movie Series Season (part of the series form) Special episode (part of the series form) Miniseries Franchise All list items can contain posters. None...","categories": [],
+        "tags": [],
+        "url": "/v0.2/forms",
+        "teaser": null
+      },{
+        "title": "The Movie Form",
+        "excerpt":"The movie form is the simplest and most straightforward of the forms. Here’s how it looks: The form consists of the following sections: Header which contains the first title, the ‘close’ button, and a few other buttons if the movie is a part of a franchise (which are discussed in...","categories": [],
+        "tags": [],
+        "url": "/v0.2/movie-form",
+        "teaser": null
+      },{
+        "title": "The Series Form",
+        "excerpt":"This is a big one. It takes much more effort to add a series than it is to add a movie. Here’s how a series form looks: The series form consists of the following parts: Header which contains the first title, the ‘close’ button, and a few other buttons if...","categories": [],
+        "tags": [],
+        "url": "/v0.2/series-form",
+        "teaser": null
+      },{
+        "title": "The Miniseries Form",
+        "excerpt":"The miniseries form describes series which are limited in nature. In the context of this app, a miniseries has only one season, which in turn has only one release period, and no special episodes. Here’s how a miniseries form looks: As you can see, the miniseries forms are much simpler...","categories": [],
+        "tags": [],
+        "url": "/v0.2/miniseries-form",
+        "teaser": null
+      },{
+        "title": "The Franchise Form",
+        "excerpt":"Lots of movies and series have multiple parts, so it’s logical to have these movies/series be grouped together in the list. This is just what franchises are for: grouping related movies, series or even other franchises. Franchises can be created and edited via the franchise forms, but those can’t be...","categories": [],
+        "tags": [],
+        "url": "/v0.2/franchise-form",
+        "teaser": null
+      },{
+        "title": "Searching, Filtering, Sorting",
+        "excerpt":"Searching You can search for certain items in the list. To do that, specify the filters, and press ‘Find next’ or ‘Find previous’. The found items are colored light purple, and the currently selected found item is colored a slightly darker shade of purple. Here’s how it looks: When the...","categories": [],
+        "tags": [],
+        "url": "/v0.2/search-filter-sort",
+        "teaser": null
+      },{
+        "title": "Preferences",
+        "excerpt":"The final feature covered in these docs is the app-wide preferences. You can open them through the ‘Edit &gt; Preferences’ menu item, or by pressing Ctrl+Shift+P, and close them by clicking the ‘close’ button in the tab header, through the ‘File &gt; Close’ menu item, or by pressing Ctrl+W. Here’s...","categories": [],
+        "tags": [],
+        "url": "/v0.2/preferences",
+        "teaser": null
+      },{
+        "title": "Version History",
+        "excerpt":"This article provides info on what’s new in every version of the MovieList app. Version 0.2 This version adds searching, filtering, and sorting capabilities to the app. Also, tags can now be added to movies and series to make searching/filtering easier. This release is still very incomplete and far from...","categories": [],
+        "tags": [],
+        "url": "/v0.2/version-history",
+        "teaser": null
+      },{
+        "title": "Technical Stuff",
+        "excerpt":"The Movie List app is written with C# and .NET 5. Following is the list of technologies used in this app and some other technical aspects. UI This app is a desktop app. I’ve contemplated writing a web app, but decided against it, partly because it’s easier, and because it...","categories": [],
+        "tags": [],
+        "url": "/v0.2/technical",
+        "teaser": null
+      },{
+        "title": "About Movie List",
+        "excerpt":"A long time ago I started keeping a list of movies and TV series I’ve watched and would like to watch. Keeping track of all these movies and series in a Word document because cumbersome since now there are more than a thousand items (I watch a lot of movies)....","categories": [],
+        "tags": [],
+        "url": "/v0.2/about",
+        "teaser": null
+      }]
