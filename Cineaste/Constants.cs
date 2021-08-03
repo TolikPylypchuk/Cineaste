ebf0fe632c011861bced6b1a8367d465d508d4ef
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2021-08-04 00:40:21 +0300

    Change the list file extension to .cnl
    cnl (Cineaste list) is nicer than clst (which means the same thing).

--- Cineaste/Constants.cs
+++ Cineaste/Constants.cs
@@ -2,7 +2,7 @@ namespace Cineaste
 {
     public static class Constants
     {
-        public const string ListFileExtension = "clst";
+        public const string ListFileExtension = "cnl";
 
         public const string LogTemplate =
             "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message}{NewLine}{Exception}";
