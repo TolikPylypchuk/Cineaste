7fa2fc415f7b1e87eed1f34d33c02cbd2b0db2ba
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2022-04-23 18:13:13 +0300

    Add an empty Blazor project
    GH-80

---
+++ Cineaste/Server/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+} else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
