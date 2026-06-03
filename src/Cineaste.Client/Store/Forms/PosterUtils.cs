a57ae61dd9478dbe412303db4b5493556107d57d
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2026-06-03 23:46:54 +0300

    Upgrade Refit to fix a certificate issue

--- src/Cineaste.Client/Store/Forms/PosterUtils.cs
+++ src/Cineaste.Client/Store/Forms/PosterUtils.cs
@@ -3,7 +3,7 @@ namespace Cineaste.Client.Store.Forms;
 public static class PosterUtils
 {
     public static string GetPosterLocation(IApiResponse response) =>
-        response.Headers.Location is { } location
+        response.Headers?.Location is { } location
             ? location.ToString()
             : throw new InvalidOperationException("The poster response must have the Location header");
 }
